146e3bf001efe70f1dee9768e422099de4af585d
Darren Anderson <dranders@qt3.io>
2021-08-31 06:59:00 -0700

    parse numbers from CSV

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import debugFn from 'debug';
 
 import csvReadDefs from './lib/csv-read-defs.mjs';
 import csvReadRecs from './lib/csv-read-recs.mjs';
+import drawChart from './lib/draw-chart.mjs';
 
 const debug = debugFn('index');
 
@@ -15,3 +16,12 @@ const {
 const recs = csvReadRecs(RECS_FILE_PATH);
 const fieldDefs = csvReadDefs(FIELD_DEFS_FILE_PATH);
 const jurisdictionDefs = csvReadDefs(JURISDICTION_DEFS_FILE_PATH);
+
+const filterByJurisdiction = (rec, jurisdictionId) => {
+    // debug(jurisdictionId);
+    return rec[0].split('/')[0] === jurisdictionId;
+};
+
+jurisdictionDefs.forEach((jurisdictionDef) => {
+    const svg = drawChart({ jurisdictionDef, recs: recs.filter((rec) => filterByJurisdiction(rec, jurisdictionDef.id)) });
+});
