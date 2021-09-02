064ee6adc182971b79f94193f8ff5b54b9c8283c
Darren Anderson <dranders@qt3.io>
2021-09-01 17:58:37 -0700

    suppress markers for missing values

--- draw-chart.mjs
+++ draw-chart.mjs
@@ -9,7 +9,7 @@ const debug = debugFn('lib:draw-chart');
 const [width, height] = [720, 540];
 // const [width, height] = [960, 720];
 
-const margin = ({top: 20, right: 20, bottom: 30, left: 40});
+const margin = ({top: 60, right: 20, bottom: 100, left: 80});
 
 const endYear = 2020;
 
@@ -38,15 +38,18 @@ const drawChart = (props) => {
 
     const [x0, x1] = [deforestationRateArray[0][0], endYear];
 
-    const [y0, y1] = d3.extent(deforestationRateArray.map(pair => pair[1]));
+    let [y0, y1] = d3.extent(deforestationRateArray.map(pair => pair[1]));
+    
+    [y0, y1] = [d3.min([y0, 0]), y1];
 
     const x = d3.scaleLinear()
         .domain([x0, x1])
         .range([margin.left, width - margin.right])
 
     const y = d3.scaleLinear()
-        .domain([d3.min([y0, 0]), y1])
-        .range([height - margin.bottom, margin.top]);
+        .domain([y0, y1])
+        .range([height - margin.bottom, margin.top])
+        .nice();
 
     const xAxis = (g) => g
         .attr("transform", `translate(0,${height - margin.bottom})`)
@@ -55,7 +58,7 @@ const drawChart = (props) => {
     const yAxis = (g) => g
         .attr("transform", `translate(${margin.left},0)`)
         .call(d3.axisLeft(y).ticks(null, "s"))
-        // .call(g => g.select(".domain").remove())
+        .call(g => g.select(".domain").remove())
 
     svg.append('rect')
         .attr("width", width)
@@ -71,7 +74,7 @@ const drawChart = (props) => {
         .attr("stroke-width", 1.5)
         .attr("fill", "none")
         .selectAll("circle")
-        .data(deforestationRateArray)
+        .data(deforestationRateArray.filter(mItem => mItem[1] !== null))
         .join("circle")
             .attr("cx", d => x(d[0]))
             .attr("cy", d => y(d[1]))
